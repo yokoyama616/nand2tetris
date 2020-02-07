9e0277e10f44aafd0dd0cd3676899b6fac057015
yokoyama <s.yokoyama@atomitech.jp>
2020-02-07 20:44:37 +0900

    ref import

--- src/CompilationEngine.java
+++ src/CompilationEngine.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
